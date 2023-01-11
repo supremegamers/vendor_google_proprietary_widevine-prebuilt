48d1076a570837be6cdce8252d5d143363e37cc1
hmtheboy154 <buingoc67@gmail.com>
2023-01-11 08:52:47 -0500

    widevine.mk : add libwvdrmengine and libwvhidl

--- widevine.mk
+++ widevine.mk
@@ -1,2 +1,3 @@
 #Include Widevine
-PRODUCT_PACKAGES += android.hardware.drm@1.3-service-lazy.widevine
+PRODUCT_PACKAGES += android.hardware.drm@1.3-service-lazy.widevine \
+					libwvdrmengine libwvhidl
